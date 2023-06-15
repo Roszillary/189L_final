bff30416a7026313c21d370b794fb78dccd36c17
Steven Gong <120246964+Roszillary@users.noreply.github.com>
2023-06-14 17:55:36 -0700

    Update All

--- 189L_final_rough/Assets/Scripts/EnemyController.cs
+++ 189L_final_rough/Assets/Scripts/EnemyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Enemy.Command;
 
 public class EnemyController : MonoBehaviour
 {
@@ -8,27 +9,40 @@ public class EnemyController : MonoBehaviour
 
     private float maxHealth;
     private float health;
+    private IEnemyCommand ability;
+    [SerializeField] private float cooldown;
+    private float cooldownTimer;
 
     void Start()
     {
+        this.cooldownTimer = Mathf.Infinity;
+
         this.maxHealth = 100.0f;
         this.health = 100.0f;
+
+        this.ability = this.GetComponent<EnemyCounter1>();
 
         healthBar.SetMaxHealth(maxHealth);
     }
 
     void Update()
     {
-        
+        this.cooldownTimer += Time.deltaTime;
+
+        if(this.cooldownTimer >= this.cooldown)
+        {
+            this.cooldownTimer = 0.0f;
+
+            this.ability.Execute(this.gameObject);
+        }
     }
 
     public void TakeDamage(float damage)
     {
+
         this.health -= damage;
         healthBar.SetHealth(health);
-
-        Debug.Log(this.health);
-
+        
         if(this.health <= 0.0f)
         {
             Destroy(this.gameObject);
---
+++ 189L_final_rough/Assets/Scripts/EnemyCounter1.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enemy.Command;
+
+namespace Enemy.Command
+{
+    public class EnemyCounter1 : MonoBehaviour, IEnemyCommand
+    {
+        [SerializeField] private float damage;
+        [SerializeField] private float projectileSpeed;
+        [SerializeField] private Transform projPoint;
+        [SerializeField] private GameObject projectile;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        public void Execute(GameObject gameObject)
+        {
+            Debug.Log("Attack");
+            projectile.transform.position = new Vector2(projPoint.position.x, projPoint.position.y);
+            projectile.GetComponent<Projectile>().Fire(-1.0f * projectileSpeed, 0.0f);
+        }
+    }
+}
